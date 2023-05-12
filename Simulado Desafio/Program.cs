08dd77228d37eeabab1669360e4ff84122045f40
Nadia Castro <nadianaely32@gmail.com>
2023-05-12 15:34:27 -0300

    Adicionar arquivos de projeto.

---
+++ Simulado Desafio/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simulado_Desafio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1 - Desafio de jogo da forca
+            string palavra;
+
+            Console.WriteLine("Digite uma palavra aleatória");
+            palavra = Console.ReadLine();
+            Console.WriteLine(palavra);
+        }
+    }
+}
